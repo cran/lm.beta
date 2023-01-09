e003c8a3ed4a86facd4a88044d432aad18d5ae4b
Stefan Behrendt <r@behrendt-stefan.de>
2023-01-09 12:00:02 +0000

    version 1.7-1

--- R/lm.beta.R
+++ R/lm.beta.R
@@ -1,8 +1,52 @@
 lm.beta <- function(object, complete.standardization = FALSE) {
-  if (!("lm" %in% attr(object, "class"))) stop("object has to be of class lm")
-  i <- if (!complete.standardization) attr(attr(object$model, "terms"), "intercept") else 1
-  object$standardized.coefficients <- coef(object) * apply(as.matrix(model.matrix(object)), 2, function(x) sqrt(sum((x - mean(x, na.rm = T) * i)^2, na.rm = T))) / apply(as.matrix(model.frame(object)[, 1]), 2, function(x) sqrt(sum((x - mean(x, na.rm = T) * i)^2, na.rm = T)))
+  # stop if not lm-class
+  # --> v2 will support selected glm's
+  # --> v2 will support selected robust estimators
+  if (!("lm" %in% attr(object, "class"))) stop("'object' has to be of class 'lm'")
+
+  # set scaling for models without intercept -- see help page
+  if (complete.standardization) {
+    i <- 1
+  } else {
+    i <- attr(attr(object$model, "terms"), "intercept")
+  }
+
+  # determine n
+  n <- nrow(object$model)
+
+  # extract weights from 'object'
+  if (exists("weights", object)) {
+    w <- object$weights
+  } else {
+    w <- rep(1, n)
+  }
+
+  # determine weighted n
+  nw <- sum(w != 0, na.rm = T)
+
+  # extract LHS from 'object'...
+  y <- as.matrix(model.frame(object)[, 1])
+  # ... and determine its standard deviation
+  sy <- sqrt(apply(y, 2,
+                   function(a) {
+                     sum(w * (a - weighted.mean(a, w, na.rm = T) * i) ^ 2, na.rm = T)
+                   }) / ((nw - 1) / nw * sum(w, na.rm = T)))
+
+  # extract RHS from 'object'...
+  x <- as.matrix(model.matrix(object))
+  # ... and determine its standard deviation
+  sx <- sqrt(apply(x, 2,
+                   function(a) {
+                     sum(w * (a - weighted.mean(a, w, na.rm = T) * i) ^ 2, na.rm = T)
+                   }) / ((nw - 1) / nw * sum(w, na.rm = T)))
+
+  # estimate standardized coefficients
+  object$standardized.coefficients <- coef(object) * sx / sy
+
+  # set standardized intercept to 'NA'
   if (attr(attr(object$model, "terms"), "intercept") == 1) object$standardized.coefficients[1] <- NA
+
+  # set class and return 'object'
   attr(object, "class") <- c("lm.beta", "lm")
   return(object)
 }
---
+++ R/xtable.lm.beta.R
@@ -0,0 +1,25 @@
+xtable.lm.beta <- function(x, standardized = TRUE, caption = NULL, label = NULL,
+                           align = NULL, digits = NULL, display = NULL,
+                           auto = FALSE, ...) {
+  return(xtable.summary.lm.beta(summary(x, standardized = standardized),
+                                caption = caption, label = label, align = align,
+                                digits = digits, display = display, auto = auto,
+                                ...))
+}
+
+xtable.summary.lm.beta <- function(x, caption = NULL, label = NULL, align = NULL,
+                                   digits = NULL, display = NULL, auto = FALSE,
+                                   ...) {
+  x <- data.frame(x$coef, check.names = FALSE)
+
+  class(x) <- c("xtable", "data.frame")
+  caption(x) <- caption
+  label(x) <- label
+  if(auto && is.null(align))   align   <- xalign(x)
+  if(auto && is.null(digits))  digits  <- xdigits(x)
+  if(auto && is.null(display)) display <- xdisplay(x)
+  align(x) <- switch(1+is.null(align), align, c("r","r","r","r","r","r"))
+  digits(x) <- switch(1+is.null(digits), digits, c(0,4,4,4,2,4))
+  display(x) <- switch(1+is.null(display), display, c("s","f","f","f","f","f"))
+  return(x)
+}
